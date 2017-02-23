c6c90d20b4efc0c17fa6114e7d24656c3d4d67fb
Micha Gorelick <mynameisfiber@gmail.com>
2017-02-23 14:55:51 -0500

    made KeepBestModel easier to use when checkpointing based on multipe metrics :rocket:

--- pymicha/keras/callbacks/keepbestmodel.py
+++ pymicha/keras/callbacks/keepbestmodel.py
@@ -8,9 +8,10 @@ class KeepBestModel(Callback):
     the best one. This will save the weight values for the best intermidate
     models and reset the model back to the best state at the end of training
     """
-    def __init__(self, monitor, how):
+    def __init__(self, monitor, how, reset_on_train_end=True):
         self.monitor = monitor
         self.how = how
+        self.reset_on_train_end = reset_on_train_end
 
     def on_train_begin(self, logs=None):
         self.best_weights = None
@@ -21,13 +22,19 @@ class KeepBestModel(Callback):
     def on_epoch_end(self, epoch, logs=None):
         current = logs.get(self.monitor)
         if self.best_value is None or self.how(current, self.best_value):
-            print("Copying model")
+            print("Copying model because of", self.monitor)
             self.best_weights = self.model.get_weights()
             self.best_value = current
             self.best_epoch = epoch + 1
 
     def on_train_end(self, logs=None):
-        print(("Resetting model to epoch {} when {} read a"
-               "value of {}").format(self.best_epoch, self.monitor,
-                                     self.best_value))
-        self.model.set_weights(self.best_weights)
+        if self.reset_on_train_end:
+            print(("Resetting model to epoch {} when {} read a"
+                   "value of {}").format(self.best_epoch, self.monitor,
+                                         self.best_value))
+            return self.reset_model(self.model)
+            self.model.set_weights(self.best_weights)
+
+    def reset_model(self, model):
+        model.set_weights(self.best_weights)
+        return model
